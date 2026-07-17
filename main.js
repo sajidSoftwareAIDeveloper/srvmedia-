aff0ee60bdc681ec4796dc86ab19395dd9db3e84
S <a>
2026-07-17 20:11:00 +0530

    First commite

---
+++ main.js
@@ -0,0 +1,5 @@
+// fetch('./components/participating-schools/index.html')
+//   .then(res => res.text())
+//   .then(html => {
+//     document.getElementById('schools-root').innerHTML = html;
+//   });
